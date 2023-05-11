da280b21f803e5177c2749cd3efc33148624384f
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-11 16:17:45 -0400

    fix redirect path in posts controller, add navbar partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,7 +24,7 @@ class PostsController < ApplicationController
   def require_login
     unless user_signed_in?
       flash[:error] = "You must sign in to add a post."
-      redirect_to user_registration_path
+      redirect_to "/users/sign_in"
     end
   end
 
