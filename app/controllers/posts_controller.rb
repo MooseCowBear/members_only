ec8d5cb6aba75a9d370bd09d55806868b188c14e
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-11 15:42:40 -0400

    fix post controller create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,10 +10,10 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
-
+    @post = current_user.posts.new(post_params)
+ 
     if @post.save
-      redirect_to posts_path #does this work?
+      redirect_to posts_path #redirects to index
     else
       render :new, status: :unprocessable_entity
     end
