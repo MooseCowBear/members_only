0c8f842d14c5425c9e70b9e3c6a0d74bbe96a7a8
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-05-11 15:19:17 -0400

    add post index view, add post controller actions

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,34 @@
+class PostsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+
+  def index
+    @posts = Post.all
+  end
+  
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path #does this work?
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  
+  def require_login
+    unless user_signed_in?
+      flash[:error] = "You must sign in to add a post."
+      redirect_to user_registration_path
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "posts#index"
   resources :posts, only: [:index, :new, :create]
 end
